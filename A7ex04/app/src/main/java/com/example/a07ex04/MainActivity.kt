ba604480aa79661aed9989b266128c5075817944
EdinaldoPSJ <edinaldopsj92@gmail.com>
2023-12-20 12:36:30 -0300

    fixing proj 4

--- A7ex04/app/src/main/java/com/example/a07ex04/MainActivity.kt
+++ A7ex04/app/src/main/java/com/example/a07ex04/MainActivity.kt
@@ -2,22 +2,36 @@ package com.example.a07ex04
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.CalendarView
-import androidx.core.widget.NestedScrollView
-import android.content.Context
-import android.widget.Toast
+import android.widget.Button
+import android.widget.ScrollView
 
+/**
+ * Este trecho de código pertence à função onCreate() de uma atividade Android.
+ *
+ * Passo a passo:
+ * 1. Chama o método onCreate da classe pai para realizar inicializações necessárias.
+ * 2. Define o layout da atividade a partir do arquivo XML (activity_main.xml).
+ * 3. Encontra o botão de retorno ao topo no layout usando o seu ID.
+ * 4. Encontra a ScrollView no layout usando o seu ID.
+ * 5. Configura um listener de clique para o botão.
+ *    - Quando o botão é clicado, faz um scroll suave para o topo da ScrollView.
+ *
+ * Propósito do código:
+ * Este código implementa um botão que, quando clicado, realiza um scroll suave para
+ * o topo de uma ScrollView. O objetivo é proporcionar uma maneira fácil de retornar ao
+ * início do conteúdo visualizado na tela, além de mostrar o ScrollView para telas que ultrapassam
+ * limite.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Define o layout da atividade com base em 'activity_main.xml'
         setContentView(R.layout.activity_main)
-        // Chama a função 'exibirToast' para exibir um Toast
-        exibirToast(this, "Calendários, eeeee")
-    }
-}
 
-// Função para exibir um Toast na tela
-fun exibirToast(context: Context, mensagem: String) {
-    Toast.makeText(context, mensagem, Toast.LENGTH_SHORT).show()
+        val backButton = findViewById<Button>(R.id.buttonBackToTop)
+        val scrollView = findViewById<ScrollView>(R.id.scrollView)
+
+        backButton.setOnClickListener {
+            scrollView.smoothScrollTo(0,0)
+        }
+    }
 }
