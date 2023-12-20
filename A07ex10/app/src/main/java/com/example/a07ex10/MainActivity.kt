ff0a936fb85d58762b0dc4f54b6259a3da500d54
EdinaldoPSJ <edinaldopsj92@gmail.com>
2023-12-20 14:14:55 -0300

    fixing proj 10

--- A07ex10/app/src/main/java/com/example/a07ex10/MainActivity.kt
+++ A07ex10/app/src/main/java/com/example/a07ex10/MainActivity.kt
@@ -9,31 +9,48 @@ import android.content.Context
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    /**
+     * Este trecho de código pertence à MainActivity em uma aplicação Android com integração ao Google Maps.
+     *
+     * Passo a passo:
+     * 1. Declaração da classe MainActivity que estende AppCompatActivity e implementa OnMapReadyCallback.
+     * 2. Declaração de uma propriedade privada lateinit mMap para representar o GoogleMap.
+     * 3. No método onCreate, chama o método da classe pai para realizar inicializações necessárias.
+     *    - Define o layout da atividade a partir do arquivo XML (activity_main.xml).
+     *    - Obtém uma referência ao SupportMapFragment no layout usando o ID 'map'.
+     *    - Chama o método getMapAsync() no SupportMapFragment para registrar a MainActivity como
+     *      um callback a ser notificado quando o GoogleMap estiver pronto.
+     * 4. No método onMapReady, é chamado quando o GoogleMap está pronto para uso.
+     *    - Inicializa a propriedade mMap com a instância de GoogleMap fornecida como parâmetro.
+     *    - Exibe um Toast informativo "Busque sua localizacao".
+     * 5. A função showToast é declarada para exibir um Toast com a mensagem fornecida.
+     *
+     * Propósito do código:
+     * Este código integra o Google Maps em uma aplicação Android.
+     * Quando a atividade é criada, ela obtém a referência ao SupportMapFragment
+     * e registra a MainActivity como um callback para ser notificado quando o GoogleMap estiver pronto.
+     * Quando o GoogleMap está pronto, é exibido um Toast informativo.
+     */
 
     private lateinit var mMap: GoogleMap
 
-    // Método chamado quando a atividade é criada
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // SupportMapFragment avisará quando o mapa estiver pronto para uso.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    // Callback chamado quando o mapa está pronto para uso
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Chama a função 'exibirToast' para exibir um Toast
-        exibirToast(this, "Abriu o mapa!!!!")
+        showToast(this, "Busque sua localizacao")
     }
 }
 
-// Função para exibir um Toast na tela
-fun exibirToast(context: Context, mensagem: String) {
+fun showToast(context: Context, mensagem: String) {
     Toast.makeText(context, mensagem, Toast.LENGTH_SHORT).show()
 }
 
