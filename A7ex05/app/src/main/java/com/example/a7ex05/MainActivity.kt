a9ff41b840bbc3bd267b526f77c42ea7937c3aa6
EdinaldoPSJ <edinaldopsj92@gmail.com>
2023-12-20 12:46:21 -0300

    fixing proj 5

--- A7ex05/app/src/main/java/com/example/a7ex05/MainActivity.kt
+++ A7ex05/app/src/main/java/com/example/a7ex05/MainActivity.kt
@@ -7,7 +7,22 @@ import com.bumptech.glide.Glide
 import android.content.Context
 import android.widget.Toast
 
-
+/**
+ * Este trecho de código pertence à função onCreate() de uma atividade Android.
+ *
+ * Passo a passo:
+ * 1. Chama o método onCreate da classe pai para realizar inicializações necessárias.
+ * 2. Define o layout da atividade a partir do arquivo XML (activity_main.xml).
+ * 3. Encontra a ImageView no layout usando o seu ID.
+ * 4. Utiliza a biblioteca Glide para carregar uma imagem remota na ImageView.
+ *    - A URL da imagem é especificada no método .load().
+ * 5. Chama a função showText para exibir um Toast com a mensagem "Puuuudim".
+ *
+ * Propósito do código:
+ * Este código carrega uma imagem remota em uma ImageView usando a biblioteca Glide,
+ * exibindo simultaneamente um Toast com a mensagem "Puuuudim". O objetivo é demonstrar
+ * o carregamento de imagens assíncrono e a exibição de mensagens breves na interface.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,19 +30,14 @@ class MainActivity : AppCompatActivity() {
 
         var imageView = findViewById<ImageView>(R.id.imgFoto)
 
-        Glide.with(this) // Inicializa o carregamento de imagem
+        Glide.with(this)
             .load("https://static.itdg.com.br/images/360-240/d1307a2e17cda187df76b78cfd3ac464/shutterstock-2322251819-1-.jpg")
-            // Carrega a imagem da URL
-            .into(imageView) // Exibe a imagem no ImageView
+            .into(imageView)
 
-        // Chama a função 'exibirToast' para exibir um Toast
-        exibirToast(this, "Puuuudim")
-
+        showText(this, "Puuuudim")
     }
 }
 
-// Função para exibir um Toast na tela
-fun exibirToast(context: Context, mensagem: String) {
-    // Cria um Toast com a mensagem fornecida e exibe-o na tela
-    Toast.makeText(context, mensagem, Toast.LENGTH_SHORT).show()
+fun showText(context: Context, message: String) {
+    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
 }
