426db08b4c4280c7c3bd71627368b1bf7f603b49
EdinaldoPSJ <edinaldopsj92@gmail.com>
2023-12-20 14:18:52 -0300

    fixing proj 11_12

--- A07ex11_12/app/src/main/java/com/example/a07ex11/MainActivity.kt
+++ A07ex11_12/app/src/main/java/com/example/a07ex11/MainActivity.kt
@@ -18,6 +18,32 @@ import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    /**
+     * Este trecho de código pertence à MainActivity em uma aplicação Android com integração ao Google Maps.
+     *
+     * Passo a passo:
+     * 1. Declara as propriedades lateinit mapView e lateinit googleMap para representar o MapView e GoogleMap.
+     * 2. No método onCreate, chama o método da classe pai para realizar inicializações necessárias.
+     *    - Define o layout da atividade a partir do arquivo XML (activity_main.xml).
+     *    - Inicializa mapView com a referência ao MapView no layout.
+     *    - Chama o método onCreate() do mapView e registra um callback para notificar quando o GoogleMap estiver pronto.
+     * 3. No método onMapReady, é chamado quando o GoogleMap está pronto para uso.
+     *    - Inicializa a propriedade googleMap com a instância de GoogleMap fornecida como parâmetro.
+     *    - Chama setupButtonClickListeners para configurar listeners de clique para os botões na interface.
+     * 4. A função setupButtonClickListeners configura listeners de clique para os botões "Home", "School" e "Work".
+     *    - Cada botão exibe um Toast informativo e coloca um marcador aleatório no mapa com um título específico.
+     * 5. A função placeRandomMarker coloca um marcador no mapa com uma posição aleatória próxima a Barretos.
+     *    - Gera uma posição aleatória em um raio de 3000 metros em relação ao centro de Barretos.
+     *    - Adiciona um marcador no mapa com a posição gerada e ajusta a câmera para focar nesse marcador.
+     * 6. Os métodos de ciclo de vida do MapView são implementados para garantir o gerenciamento correto do ciclo de vida.
+     *    - onStart, onResume, onPause, onStop, onDestroy, onSaveInstanceState e onLowMemory.
+     * 7. A função showMessage exibe um Toast com a mensagem fornecida.
+     *
+     * Propósito do código:
+     * Este código integra o Google Maps em uma aplicação Android.
+     * A atividade contém um MapView que, quando criado, inicializa o GoogleMap e configura botões
+     * para adicionar marcadores aleatórios no mapa, cada um representando um local específico (casa, escola, trabalho).
+     */
 
     private lateinit var mapView: MapView
     private lateinit var googleMap: GoogleMap
@@ -26,66 +52,49 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Inicialização do MapView
         mapView = findViewById(R.id.mapView)
         mapView.onCreate(savedInstanceState)
         mapView.getMapAsync(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        // Callback chamado quando o mapa está pronto para uso
         this.googleMap = googleMap
-
-        // Configura os listners de clique dos botões
         setupButtonClickListeners()
     }
 
     private fun setupButtonClickListeners() {
-        // Configura os ouvintes de clique para os botões do aplicativo
         findViewById<Button>(R.id.buttonHome).setOnClickListener {
-            // Chama a função 'exibirToast' para exibir um Toast
-            exibirToast(this, "Bora ir para casa!!!!")
-            // Coloca um marcador aleatório com o título "Casa" no mapa
+            showMessage(this, "Bora ir para casa!!!!")
             placeRandomMarker("Casa")
         }
 
         findViewById<Button>(R.id.buttonSchool).setOnClickListener {
-            // Chama a função 'exibirToast' para exibir um Toast
-            exibirToast(this, "É hora de estudar!!!!")
-            // Coloca um marcador aleatório com o título "Escola" no mapa
+            showMessage(this, "É hora de estudar!!!!")
             placeRandomMarker("Escola")
         }
 
         findViewById<Button>(R.id.buttonWork).setOnClickListener {
-            // Chama a função 'exibirToast' para exibir um Toast
-            exibirToast(this, "Uhuuu, hora do trabalho!!!!")
-            // Coloca um marcador aleatório com o título "Trabalho" no mapa
+            showMessage(this, "Uhuuu, hora do trabalho!!!!")
             placeRandomMarker("Trabalho")
         }
     }
 
     private fun placeRandomMarker(title: String) {
-        // Gera uma localização aleatória próxima a Barretos-SP
         val randomLatLng = getRandomLocationNearBarretos()
 
-        // Adiciona um marcador no mapa com a posição e título especificados
         googleMap.addMarker(MarkerOptions().position(randomLatLng).title(title))
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(randomLatLng, 15f))
     }
 
-    // Função para gerar uma localização aleatória próxima a Barretos-SP
     private fun getRandomLocationNearBarretos(): LatLng {
-        // Centro aproximado de Barretos-SP
         val center = LatLng(-20.5587, -48.5679)
 
-        // Raio em metros para geração de pontos aleatórios
         val radius = 3000
         val random = Random
 
         val x0 = center.longitude
         val y0 = center.latitude
 
-        // Converte o raio de metros para graus
         val radiusInDegrees = radius / 111320f
 
         val u = random.nextDouble()
@@ -140,9 +149,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 }
 
-// Função para exibir um Toast na tela
-fun exibirToast(context: Context, mensagem: String) {
-    Toast.makeText(context, mensagem, Toast.LENGTH_SHORT).show()
+fun showMessage(context: Context, message: String) {
+    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
 }
 
 
